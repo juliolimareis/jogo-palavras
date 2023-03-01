ea2ab656d2365d547f91a11fb2dded2179bb0cfb
Júlio César Lima Reis <juliolimareis@gmail.com>
2023-03-01 00:24:57 -0300

    Feature: Add nova validação de palavras

--- server/api/check-word/[word].ts
+++ server/api/check-word/[word].ts
@@ -1,29 +1,21 @@
+import DictionaryBr from "~~/game/dictionary-br.json";
+import DictionaryEn from "~~/game/dictionary-en.json";
+
 export default defineEventHandler(async event => {
   const word = decodeURIComponent(event.context.params.word);
+  const query = getQuery(event);
+
   let isValid = false;
 
-  return await new Promise<{ isValid: boolean, word: string }>((resolve, reject) => {
-    fetch(`https://api.dicionario-aberto.net/near/${word}`)
-      .then(res => resolve(res.json()))
-      .catch(err => {
-        event.node.req.statusCode = 408;
-        reject(err);
-      });
-
-    setTimeout(() => {
-      event.node.req.statusCode = 408;
-      reject(`[check-word]: https://api.dicionario-aberto.net/near/${word}: timeout`);
-    }, 10000);
-  }).then(res => {
-    if(res && Array.isArray(res)){
-      isValid = res.some(w => w === word);
+  if(query.lang === "pt"){
+    if(DictionaryBr.includes(word)){
+      isValid = true;
     }
+  }else if(query.lang === "en"){
+    if(DictionaryEn.includes(word)){
+      isValid = true;
+    }
+  }
 
-    return { isValid, word };
-  }).catch(err => {
-    event.node.req.statusCode = 408;
-    console.log(err);
-
-    return { isValid, word };
-  });
+  return { isValid, word };
 });
